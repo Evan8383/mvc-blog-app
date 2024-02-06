a7a2cc8548543bb578973b12b64d950b1cc372c3
Evan8383 <evanbennett83@gmail.com>
2024-02-06 14:25:43 -0500

    added dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/withAuth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('edit-post', {
+      post,
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post');
+});
+
+module.exports = router;
